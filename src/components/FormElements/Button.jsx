76a20a9b5fc6b7802406ca40a17f316af5ef3e2b
eshwagadkar <eshwagadkar007@gmail.com>
2026-08-19 17:10:26 +0530

    feat: create a reusable button

---
+++ src/components/FormElements/Button.jsx
@@ -0,0 +1,17 @@
+export default function Button({ 
+    children,
+    onClick, 
+    type='button', 
+    disabled=false,
+    className }) {
+    return (
+        <button
+            className={className}
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            >
+            {children}
+        </button>
+    )
+}
